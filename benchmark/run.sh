571b120d0efa04346fa6fddc82c8cf076a6ca500
pierreozoux <pierre@ozoux.net>
2018-05-30 15:18:53 +0200

    ok

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash -eux
 
-fio /app/eval.fio
+echo ${BUCKET}
+echo ${VOLUME_TYPE}
+mkdir /tmp/io
+cp /app/eval.fio /tmp/io/eval.fio
+cd /tmp/io
+
+fio ./eval.fio
 fio2gnuplot -t Bandwitdh -b -g
 fio2gnuplot -t iops -i -g
 fio2gnuplot -t lat -g -p '*_lat*'
-python -m SimpleHTTPServer 8000
+
+s3cmd put --recursive /tmp/io s3://${BUCKET}/${VOLUME_TYPE}/`date +%Y_%m_%d-%H_%M_%S`/
+
+#python -m SimpleHTTPServer 8000
